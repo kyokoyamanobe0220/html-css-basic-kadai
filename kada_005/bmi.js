ddbc4ee346387528c3a0f4046c61c176bc791dce
山辺　恭子 <150514388+kyokoyamanobe0220@users.noreply.github.com>
2024-03-29 14:25:13 +0900

    Add files via upload
    kadai005のアップ

---
+++ kada_005/bmi.js
@@ -0,0 +1,18 @@
+//変数の宣言(体重)
+let bodyweight;
+//値の代入(体重)
+bodyweight = 68;
+
+//変数の宣言(身長)
+let height;
+//値の代入(身長）
+height = 1.7;
+
+
+// 変数の宣言（BMI計算式)
+let bmi;
+// 値の代入（BMI計算式）
+bmi = bodyweight / (height * height);
+
+//コンソールへの出力
+console.log(bmi)
